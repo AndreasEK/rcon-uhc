0004ca4cfc0c027330514bc950bfb3d959c92d5c
Andreas Ebbert-Karroum <andreas@karroum.de>
2017-04-26 21:46:28 +0200

    Event 2: Ein goldener Apfel für einen Spieler aus jedem Team

--- Event_2.sh
+++ Event_2.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 source env.sh
-echo "EVENT 2 IS RUNNING"
+
 sleep $EFFECT_WAIT
+
+echo "EVENT: Güldene Äpfel"
+$MCRCON_HOME/mcrcon -H $SERVER_IP -p $PASSWD "title @a title {\"text\":\"Güldene Äpfel\", \"color\":\"yellow\"}" "title @a subtitle {\"text\":\"Ein goldener Apfel für einen Spieler pro Team\"}"
+
+# Gibt einem Spieler eine verwunschene und recht stark abgenutze Diamant-Brustplate,
+# die nicht abgelegt werden kann (curse of binding) und bei Tod verschwindet
+# (curse of vanishing).
+# Diese Ruestung limitiert die maximalen hitpoints auf 7 Herzen, so lange sie getragen wird, und nicht durch Tod oder Abnutzung zerstoert wird.
+for team in blue gold green aqua red yellow light_purple dark_blue
+do
+	$MCRCON_HOME/mcrcon -H $SERVER_IP -p $PASSWD "give @r[team=$team] golden_apple"
+done
