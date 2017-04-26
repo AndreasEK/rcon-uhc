9123892fb566aa0b124fe48d70e33265e23b202e
Andreas Ebbert-Karroum <andreas@karroum.de>
2017-04-26 23:40:58 +0200

    Event 3: Töte alle Mobs im Umkreis von 20 Blöcken um jeden Spieler

--- Event_3.sh
+++ Event_3.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 source env.sh
-echo "EVENT 3 IS RUNNING"
+
 sleep $EFFECT_WAIT
+
+echo "EVENT: Tod den Mobs"
+$MCRCON_HOME/mcrcon -H $SERVER_IP -p $PASSWD "title @a title {\"text\":\"Tod den Mobs\", \"color\":\"green\"}" "title @a subtitle {\"text\":\"Alle Mobs im Umkreis von 20 Blöcken um jeden Spieler sterben.\"}"
+
+$MCRCON_HOME/mcrcon -H $SERVER_IP -p $PASSWD "/execute @a[type=player,m=0] ~ ~ ~ /kill @e[type=!player,r=20]"
+
