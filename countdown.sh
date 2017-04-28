44af03dbf6362b8cdb1ca8502ecfa424ce68dfd3
micheldrescher <Michel.Drescher@oerc.ox.ac.uk>
2017-04-28 11:36:44 +0000

    Parametrised countdown script

--- countdown.sh
+++ countdown.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #########################################################
-#               	countdown.sh                        #
+#                       countdown.sh                    #
 #                                                       #
 # Runs a countdown timer on the server.                 #
 #                                                       #
 # Arguments - for now all arguments are mandatory:      #
-#	numseconds - Number of seconds to count down        #
+#   numseconds - Number of seconds to count down        #
 #                                                       #
 #########################################################
 
@@ -15,21 +15,19 @@ source env.sh
 #
 # TODO - proper check of command line arguments
 #
-if [ $# -ne 1 ] then
+if [ $# -ne 1 ] 
+then
 	echo "Usage: countdown.sh <numseconds>"
 	exit 255
 fi
 
-echo $RCON_CMD 
-exit 0
-
 #COUNTDOWN
 # Reset & configure event display times 
 $PY_MCRCON $SERVER_IP $RCON_PORT $PASSWD "title @a reset"
-$PY_MCRCON $SERVER_IP $RCON_PORT $PASSWD "title @a times 0 ($1*20) 0"
+$PY_MCRCON $SERVER_IP $RCON_PORT $PASSWD "title @a times 0 20 0"
 
 #counting down
-for countdown in {$1..1}
+for ((countdown = $1; countdown>= 1; countdown--))
 do
   $RCON_CMD "title @a title {\"text\":\"$countdown\"}"
   sleep 1
