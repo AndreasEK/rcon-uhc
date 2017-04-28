9b6eb69a24ea8c993f189a80e4508be17f246a89
micheldrescher <Michel.Drescher@oerc.ox.ac.uk>
2017-04-28 13:44:50 +0100

    Porting event 6

--- Event_6_Thunderstorm.sh
+++ Event_6_Thunderstorm.sh
@@ -1,13 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+
+#########################################################
+#                Event_6_Thunderstorm.sh                #
+#                                                       #
+# Thunderstorm for a whole day!                         #
+#                                                       #
+# No arguments to the script.                           #
+#                                                       #
+#########################################################
+
 source env.sh
 
+# sleep for the effect wait time minus 10 seconds (for announcement and countdown)
 sleep $(( $EFFECT_WAIT - 10 ))
 
-echo "EVENT: Gewitter"
-$MCRCON_HOME/mcrcon -H $SERVER_IP -p $PASSWD "title @a title {\"text\":\"Donnergrollen\", \"color\":\"red\"}" "title @a subtitle {\"text\":\"Schlechtes Wetter, den ganzen Tag\"}"
+echo "EVENT: Gewitter einen Tag lang"
 
-sleep 5
+# show event title and subtitle for 5 seconds
+./announce.sh "Donnergrollen!" red "Schlechtes Wetter, den ganzen Tag" white 5
 
-./countdown.sh
+# countdown 5 seconds
+./countdown.sh 5
 
-$MCRCON_HOME/mcrcon -H $SERVER_IP -p $PASSWD "weather thunder 20000"
+#
+# run the event
+#
+$RCON_CMD "weather thunder 20000"
