e1388a051f4bbc4756bbc4e545587dc3d71c7abc
micheldrescher <Michel.Drescher@oerc.ox.ac.uk>
2017-04-28 13:23:54 +0100

    Porting event 3 done

--- Event_3_Death_All_Mobs.sh
+++ Event_3_Death_All_Mobs.sh
@@ -1,14 +1,29 @@
-#!/bin/sh
+#!/bin/bash
+
+############################################################
+#                 Event_2_Golden_Apples.sh                 #
+#                                                          #
+# Give one randomly picked player per team a golden apple. #
+# plate with some bad, hampering statistics.               #
+#                                                          #
+# No arguments to the script.                              #
+#                                                          #
+############################################################
+
 source env.sh
 
+# sleep for the effect wait time minus 10 seconds (for announcement and countdown)
 sleep $(( $EFFECT_WAIT - 10 ))
 
-echo "EVENT: Tod den Mobs"
-$MCRCON_HOME/mcrcon -H $SERVER_IP -p $PASSWD "title @a title {\"text\":\"Tod den Mobs\", \"color\":\"green\"}" "title @a subtitle {\"text\":\"Alle Mobs im Umkreis von 20 Blöcken um jeden Spieler sterben.\"}"
+echo "EVENT: Tod den Mobs!"
 
-sleep 5
+# show event title and subtitle for 5 seconds
+title="Tod den Mobs!"
+subtitle="Alle Mobs im Umkreis von 20 Blöcken um jeden Spieler sterben!"
+./announce.sh "$title" green "$subtitle" green 5
 
-./countdown.sh
+# countdown 5 seconds
+./countdown.sh 5
 
-$MCRCON_HOME/mcrcon -H $SERVER_IP -p $PASSWD "/execute @a[type=player,m=0] ~ ~ ~ /kill @e[type=!player,r=20]"
+$RCON_CMD "/execute @a[type=player,m=0] ~ ~ ~ /kill @e[type=!player,r=20]"
 
